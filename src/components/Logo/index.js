2e066ce9bd336638fcfc3c09a7c2aad86de2930a
Tolulope Ogunremi <to4g16@soton.ac.uk>
2021-02-09 00:27:01 +0000

    changed logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from '../../images/logo-iceberg.svg';
+import logo from '../../images/logoakwaaba.png';
 
 const Logo = () => {
-  return <img src={logo} alt="Iceberg Logo" />;
+  return <img src={logo} alt="Akwaaba Logo" />;
 };
 
 export default Logo;
